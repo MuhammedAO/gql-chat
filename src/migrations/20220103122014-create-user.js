8a1cfb966bf5fcfc6382454aa4a7a59ad1f0a082
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-01-03 15:46:36 +0100

    user migrations

---
+++ src/migrations/20220103122014-create-user.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      username: {
+        type:DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  down: async (queryInterface, DataTypes) => {
+    await queryInterface.dropTable('users');
+  }
+};
