5e53fd518dfb2ae0fa6c9bbb873d4db3ab382803
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-01-06 00:30:11 +0100

    additional properties

--- src/models/user.js
+++ src/models/user.js
@@ -23,6 +23,13 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
+    },
+    password: {
+    type: DataTypes.STRING,
+    allowNull: false
+    },
+    imageUrl: {
+     type: DataTypes.STRING,
     }
   }, {
     sequelize,
