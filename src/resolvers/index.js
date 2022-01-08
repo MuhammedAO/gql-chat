b2e6018377c15f731cb30ca30d8346135a9081a2
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-01-08 23:58:36 +0100

    login resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,6 +1,7 @@
 const { UserInputError } = require('apollo-server-express')
 const bcrypt = require('bcryptjs')
 const {User} =  require('../models')
+const jwt = require('jsonwebtoken')
 
 
 module.exports  = {
@@ -14,7 +15,51 @@ module.exports  = {
         console.log(error)
         throw new Error(error)
       }
-    }
+    },
+    login: async () => {
+      const { username, password } = args
+      let errors = {}
+
+      try {
+        if (username.trim() === '')
+          errors.username = 'username must not be empty'
+        if (password === '') errors.password = 'password must not be empty'
+
+        if (Object.keys(errors).length > 0) {
+          throw new UserInputError('bad input', { errors })
+        }
+
+        const user = await User.findOne({
+          where: { username },
+        })
+
+        if (!user) {
+          errors.username = 'user not found'
+          throw new UserInputError('user not found', { errors })
+        }
+
+        const correctPassword = await bcrypt.compare(password, user.password)
+
+        if (!correctPassword) {
+          errors.password = 'password is incorrect'
+          throw new AuthenticationError('password is incorrect', { errors })
+        }
+
+        const token = jwt.sign({ username }, 'JWT_SECRET', {
+          expiresIn: 60 * 60,
+        })
+
+        return {
+          ...user.toJSON(),
+          createdAt: user.createdAt.toISOString(),
+          token,
+        }
+      } catch (err) {
+        console.log(err)
+        throw err
+      }
+    },
+    
   },
   Mutation: {
     register: async (_, args) => {
